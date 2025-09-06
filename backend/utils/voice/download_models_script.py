f85732b0fc85ae293cb0b3698dd3b4f046a43a20
GMANUL <142923862+GMANUL@users.noreply.github.com>
2025-09-06 17:08:01 +0300

    quick fix silero vad download

--- backend/utils/voice/download_models_script.py
+++ backend/utils/voice/download_models_script.py
@@ -4,21 +4,23 @@ import requests
 from path_config import SILERO_TTS_MODEL_PATH, SILERO_TTS_MODEL_DIR, \
     SILERO_VAD_MODEL_DIR, SILERO_VAD_MODEL_PATH, PARAKEET_MODEL_DIR 
 
-#silero_vad
-os.makedirs(SILERO_VAD_MODEL_DIR, exist_ok=True)
-silero_vad, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
-                              model='silero_vad',
-                              force_reload=True, 
-                              trust_repo=True, 
-                              onnx=False)         
 
-torch.jit.save(silero_vad, SILERO_VAD_MODEL_PATH)
+#silero_vad
+if not os.path.isfile(SILERO_VAD_MODEL_PATH):
+    os.makedirs(SILERO_VAD_MODEL_DIR, exist_ok=True)
+    silero_vad, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+                                model='silero_vad',
+                                force_reload=True, 
+                                trust_repo=True, 
+                                onnx=False)         
+
+    torch.jit.save(silero_vad, SILERO_VAD_MODEL_PATH)
 
 
 
 #silero_tts
-os.makedirs(SILERO_TTS_MODEL_DIR, exist_ok=True)
 if not os.path.isfile(SILERO_TTS_MODEL_PATH):
+    os.makedirs(SILERO_TTS_MODEL_DIR, exist_ok=True)
     silero_tts_url = 'https://models.silero.ai/models/tts/ru/v3_1_ru.pt'
     torch.hub.download_url_to_file(silero_tts_url, SILERO_TTS_MODEL_PATH, progress=False)
 
